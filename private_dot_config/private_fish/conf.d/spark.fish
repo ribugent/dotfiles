86e9a3730a95223fece78f2301144a4f2ef860dc
Gerard Ribugent <ribugent@gmail.com>
2022-06-17 13:57:35 +0200

    Fix wrong version of elastic search spark

--- private_dot_config/private_fish/conf.d/spark.fish
+++ private_dot_config/private_fish/conf.d/spark.fish
@@ -1,2 +1,2 @@
 set -x SPARK_HOME /home/ribu/local/spark-3.2.1-bin-hadoop2.7
-set -x PYSPARK_SUBMIT_ARGS --packages io.delta:delta-core_2.12:1.2.+,org.elasticsearch:elasticsearch-spark-30_2.13:8.2.3  --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" pyspark-shell
+set -x PYSPARK_SUBMIT_ARGS --packages io.delta:delta-core_2.12:1.2.+,org.elasticsearch:elasticsearch-spark-30_2.12:8.2.3  --conf "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension" --conf "spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog" pyspark-shell
