1af86c753bfabed68496f72ad69258ea959fbd4d
Gerard Ribugent <ribugent@gmail.com>
2022-07-22 13:16:36 +0200

    Improve cd function to avoid "subshell" on cd

--- private_dot_config/private_fish/functions/cz.fish
+++ private_dot_config/private_fish/functions/cz.fish
@@ -1,3 +1,8 @@
-function cz --wraps=chezmoi --description 'alias cz chezmoi'
-  chezmoi $argv; 
+function cz --wraps=chezmoi --description 'alias cz chezmoi with \'cd\' catching'
+  switch $argv[1]
+    case 'cd'
+      cd (chezmoi source-path)
+    case '*'
+      chezmoi $argv
+  end
 end
