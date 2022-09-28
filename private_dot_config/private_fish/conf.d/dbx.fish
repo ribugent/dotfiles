b95cb410f65045c973319cb6965ca98d730d7a74
Gerard Ribugent <ribugent@gmail.com>
2022-09-28 08:44:11 +0200

    Change dbx completion

--- private_dot_config/private_fish/conf.d/dbx.fish
+++ private_dot_config/private_fish/conf.d/dbx.fish
@@ -1,1 +1,5 @@
-dbx --show-completion fish | source
+# This cannot be called due slowness
+# dbx --show-completion fish | source
+
+# Here's the output of previous command
+complete --command dbx --no-files --arguments "(env _DBX_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) dbx)" --condition "env _DBX_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) dbx"
