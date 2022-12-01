d74e6ce53e5ec4868a1598c09f94bb91942cc89a
Gerard Ribugent <ribugent@gmail.com>
2022-12-01 12:59:05 +0100

    Disable due issues with gtk and wayland

--- private_dot_config/private_fish/conf.d/vars.fish
+++ private_dot_config/private_fish/conf.d/vars.fish
@@ -2,5 +2,6 @@ set -x BROWSER xdg-open
 set -x EDITOR vim
 set -x MANPAGER 'less -R --use-color -Dd+r -Du+b'
 set -x GTK_OVERLAY_SCROLLING 0
-set -x GTK_USE_PORTAL 1
+# set -x GTK_USE_PORTAL 1
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x grc_plugin_ignore_execs df
