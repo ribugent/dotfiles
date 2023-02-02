a298951a511838be776378cc790a03208c672df5
Gerard Ribugent <ribugent@gmail.com>
2023-02-02 13:58:18 +0100

    Enable nerd fonts in theme

--- private_dot_config/private_fish/conf.d/vars.fish
+++ private_dot_config/private_fish/conf.d/vars.fish
@@ -5,6 +5,7 @@ set -x GTK_OVERLAY_SCROLLING 0
 # set -x GTK_USE_PORTAL 1
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x grc_plugin_ignore_execs df
+set -x theme_nerd_fonts yes
 
 if test "$XDG_SESSION_TYPE" = "wayland"
     set -x MOZ_ENABLE_WAYLAND 1
