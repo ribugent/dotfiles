710e95ad87210a6f534d96d28ad0bed83b8d6736
Gerard Ribugent <ribugent@gmail.com>
2023-10-10 15:07:23 +0200

    Remove dbx

--- private_dot_config/private_fish/conf.d/dbx.fish
+++
@@ -1,6 +0,0 @@
-# This cannot be called due slowness
-# See bug: https://github.com/databrickslabs/dbx/issues/494
-# dbx --show-completion fish | source
-
-# As workarround here's the output of previous command
-complete --command dbx --no-files --arguments "(env _DBX_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) dbx)" --condition "env _DBX_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) dbx"
