7c922c1384b9d5a88ae2cfd4f0ec0a8793707226
Gerard Ribugent <ribugent@gmail.com>
2023-12-15 16:17:06 +0100

    Remove spark install

--- run_once_before-install_spark.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-[ -d ~/local/spark-3.2.1-bin-hadoop2.7 ] && exit 0
-
-mkdir ~/local/spark-3.2.1-bin-hadoop2.7
-curl -L https://dlcdn.apache.org/spark/spark-3.2.1/spark-3.2.1-bin-hadoop2.7.tgz | tar -xz --strip-components=1 -C ~/local/spark-3.2.1-bin-hadoop2.7
