5aef3418cda4e252c695b4ca480120187e3f64bb
Gerard Ribugent <ribugent@gmail.com>
2024-01-31 09:53:38 +0100

    Opt out sam telemetry and reorganize variables

--- private_dot_config/private_fish/conf.d/vars.fish
+++ private_dot_config/private_fish/conf.d/vars.fish
@@ -1,17 +1,20 @@
 if test (uname) = "Linux"
     set -x BROWSER xdg-open
+    set -x GTK_OVERLAY_SCROLLING 0
+
+    if test "$XDG_SESSION_TYPE" = "wayland"
+        set -x MOZ_ENABLE_WAYLAND 1
+    end
 end
-set -x EDITOR vim
+
 if test (uname) = "Darwin"
     set -x HOMEBREW_BAT 1
 end
+
+set -x EDITOR vim
 set -x MANPAGER 'less -R --use-color -Dd+r -Du+b'
-set -x GTK_OVERLAY_SCROLLING 0
+set -x SAM_CLI_TELEMETRY 0
 # set -x GTK_USE_PORTAL 1
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x grc_plugin_ignore_execs df
 set -x theme_nerd_fonts yes
-
-if test "$XDG_SESSION_TYPE" = "wayland"
-    set -x MOZ_ENABLE_WAYLAND 1
-end
