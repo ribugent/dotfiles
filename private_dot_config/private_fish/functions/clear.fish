d650c4db8ae04e8ba58ec08cfccdb218eff486b3
Gerard Ribugent <ribugent@gmail.com>
2024-03-15 14:14:26 +0100

    Ensure clear works as Linux

---
+++ private_dot_config/private_fish/functions/clear.fish
@@ -0,0 +1,5 @@
+# macOs iTerm2 💩, clear doesn't clean the full terminal, running twice works as charm
+function clear --description 'Ensure cleans buffer in iTerm2'
+  command clear;
+  command clear
+end
