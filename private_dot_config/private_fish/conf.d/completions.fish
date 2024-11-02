aec1da99fcd45131ddacaf249875eb96fcf643f5
Gerard Ribugent <ribugent@gmail.com>
2024-11-02 20:56:32 +0100

    Enable aws completions when aws is available

--- private_dot_config/private_fish/conf.d/completions.fish
+++ private_dot_config/private_fish/conf.d/completions.fish
@@ -1,5 +1,7 @@
 # AWS completions
-complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+if type -q aws
+    complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+end
 
 if test (uname) = "Darwin"
     set -x fish_complete_path $fish_complete_path /opt/homebrew/opt/curl/share/fish/vendor_completions.d
