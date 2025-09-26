0c4113635dccbc17fa2351c93ca823b965a7acd3
Gerard Ribugent <ribugent@gmail.com>
2025-09-26 14:30:09 +0200

    Setup rustup

--- private_dot_config/private_fish/conf.d/00-vars.fish
+++ private_dot_config/private_fish/conf.d/00-vars.fish
@@ -19,6 +19,7 @@ if test (uname) = "Darwin"
     fish_add_path $HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin
     fish_add_path $HOMEBREW_PREFIX/opt/findutils/libexec/gnubin
     fish_add_path $HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin
+    fish_add_path $HOMEBREW_PREFIX/opt/rustup/bin
 end
 
 set -x EDITOR nvim
