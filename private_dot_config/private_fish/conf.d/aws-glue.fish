48cc92e1a3f6bae55d40344652fcc5fc561c2e62
Gerard Ribugent <ribugent@gmail.com>
2026-09-18 08:40:42 +0200

    Remove deprecated files

--- private_dot_config/private_fish/conf.d/aws-glue.fish
+++
@@ -1,5 +0,0 @@
-# set -x SPARK_HOME ~/local/spark-2.4.3-bin-spark-2.4.3-bin-hadoop2.8
-# set -x SPARK_HOME ~/local/spark-3.1.1-amzn-0-bin-3.2.1-amzn-3
-# set -x PYSPARK_PYTHON python3
-# set -x PYSPARK_DRIVER_PYTHON python3
-# set -x fish_user_paths ~/local/aws-glue-libs/bin ~/local/apache-maven-3.6.0/bin $fish_user_paths
